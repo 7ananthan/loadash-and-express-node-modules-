d44636723f58fa22a105fe28e93ee4aa91bed2e0
7ananthan <7ananthan@gmail.com>
2020-04-01 12:55:13 +0530

    boduparser and postman

--- app1.js
+++ app1.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var bodyParser =require('body-parser'); 
+
 var app =express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.get('/', (req, res) => {
  
@@ -7,12 +12,16 @@ app.get('/', (req, res) => {
     
    });
     
-   app.get('/gallery', (req, res) => {
-    res.send("My Gallery Page");
+   app.post('/gallery', (req, res) => {
+
+    var name =req.body.getname;
+    var roll =req.body.getroll;
+
+    res.send("hello "+ name + "  Roll no :" +roll);
    });
    
    
     
-   app.listen(3000, () => {
+   app.listen(3000, () => { 
     console.log("server started");
    });
